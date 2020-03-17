6417f2ce9b87b606c88c2c98d71ca4ef1e756493
Zachary Silvis <60155477+Bandit254@users.noreply.github.com>
2020-03-17 16:52:22 -0700

    Added project to remove the vowels from a string

---
+++ Disemvowell_Trolls/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Disemvowell_Trolls
+{
+    class Program
+    {
+        /*
+         Trolls are attacking your comment section!
+
+A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+Note: for this kata y isn't considered a vowel.
+*/
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Remove the vowels from a given string.\n");
+        }
+
+        public static string Disemvowell(string str)
+        {
+
+            return str;
+        }
+    }
+}
