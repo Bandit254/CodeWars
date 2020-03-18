2668987fadbd36aded54ada204059f69c26e0366
Zachary Silvis <60155477+Bandit254@users.noreply.github.com>
2020-03-18 12:03:41 -0700

    Added a project to determine if a string is an isogram (ignoring case)

---
+++ Isograms/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Isograms
+{
+    class Program
+    {
+        /*
+         * An isogram is a word that has no repeating letters, consecutive or non-consecutive. 
+         * Implement a function that determines whether a string that contains only letters is an isogram. 
+         * Assume the empty string is an isogram. Ignore letter case.
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Determine whether or not a word is an isogram.\n");
+            string str= "Dermatoglyphics";
+            string str1 = "aba";
+            string str2 = "moOse";
+            Console.WriteLine($"{str} is an Isogram: {IsIsogram(str)}\n");
+            Console.WriteLine($"{str1} is an Isogram: {IsIsogram(str1)}\n");
+            Console.WriteLine($"{str2} is an Isogram: {IsIsogram(str2)}\n");
+        }
+
+        public static bool IsIsogram(string str)
+        {
+            bool isIsogram = true;
+            string lower = str.ToLower();//To ignore letter case
+            for (int i = 0; i < lower.Length; i++)
+            {
+                for (int j = i+1; j < lower.Length; j++)
+                {
+                    if (String.Equals(lower[i],lower[j]))
+                    {
+                        isIsogram = false;
+                    }
+
+                }
+            }
+            return isIsogram;
+        }
+    }
+}
