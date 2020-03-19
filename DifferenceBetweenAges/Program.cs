ff736d38cb9f4cc24a6697f2ed78047e0b74b53c
Zachary Silvis <60155477+Bandit254@users.noreply.github.com>
2020-03-19 14:52:13 -0700

    Added a project to find the digital root of a number (still in progress) as well as a project to create an array of integers representing the youngest and oldest ages, plus the difference between them

---
+++ DifferenceBetweenAges/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace DifferenceBetweenAges
+{
+    class Program
+    {
+        /*
+         * At the annual family gathering, the family likes to find the oldest living family member’s age and the youngest family member’s age 
+         * and calculate the difference between them.
+           You will be given an array of all the family members' ages, in any order. 
+           The ages will be given in whole numbers, so a baby of 5 months, will have an ascribed ‘age’ of 0. 
+           Return a new array with [youngest age, oldest age, difference between the youngest and oldest age].
+           */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Given an array of ages, return a new array that lists the youngest age, oldest age, and the difference between them.\n ");
+            int[] ages = new int[] { 60, 29, 62, 33, 27, 24 };
+            int[] differenceBetweenAges = DifferenceInAges(ages);
+            PrintArray(differenceBetweenAges);
+
+
+        }
+
+        public static int[] DifferenceInAges(int[] ages)
+        {
+            int[] differenceArray = new int[3];
+            Array.Sort(ages);
+            int youngest = ages[0];
+            int oldest = ages[ages.Length - 1];
+            int diffAges = oldest - youngest;
+            differenceArray[0] = youngest;
+            differenceArray[1] = oldest;
+            differenceArray[2] = diffAges;
+
+            return differenceArray;
+        }
+
+        public static void PrintArray(int[] inputArray)
+        {
+            foreach (int x in inputArray)
+            {
+                Console.Write($"{x} ");
+            }
+        }
+    }
+}
---
+++ DigitalRoot/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace DigitalRoot
+{
+    class Program
+    {
+        /*
+          In this kata, you must create a digital root function.
+        A digital root is the recursive sum of all the digits in a number. 
+        Given n, take the sum of the digits of n. If that value has more than one digit, 
+        continue reducing in this way until a single-digit number is produced. 
+        This is only applicable to the natural numbers.
+        */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a function that calculates the digital root (sum of digits) of a given number).\n");
+            long digRoot1 = 16;
+            long digRoot2 = 942;
+            long digRoot3 = 132189;
+            long digRoot4 = 493193;
+            Console.WriteLine($"The digital root of {digRoot1} is {DigitalRoot(digRoot1)}.\n");
+            Console.WriteLine($"The digital root of {digRoot2} is {DigitalRoot(digRoot2)}.\n");
+            Console.WriteLine($"The digital root of {digRoot3} is {DigitalRoot(digRoot3)}.\n");
+            Console.WriteLine($"The digital root of {digRoot4} is {DigitalRoot(digRoot4)}.\n");
+
+
+        }
+
+        public static long DigitalRoot(long n)
+        {
+            long digitalRoot=0;
+            //int digitalRootSum=0;
+            while (n > 0) 
+            {
+                digitalRoot += (n % 10);
+                n = n / 10;
+            }
+            while (digitalRoot>9)
+            {
+                DigitalRoot(digitalRoot);
+            }
+            
+            //digitalRoot = 0;
+            /*
+            if (digitalRoot>=10)
+            {
+                DigitalRoot(digitalRoot);
+            }
+            else if (digitalRoot<10)
+            {
+                digitalRootSum = (int)digitalRoot;
+                
+            }
+            */
+            return digitalRoot;
+           
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public static int SumOfDigits(long n)
+        {
+            
+            int digitalRoot = 0;
+            double x = (double)n;
+            string inputNumberString = x.ToString();
+            int[] numArray = new int[inputNumberString.Length];
+            for (int i = 0; i < inputNumberString.Length; i++)
+            {
+                numArray[i] = (int)inputNumberString[i];
+            }
+            foreach (int i in numArray)
+            {
+                digitalRoot += 1;
+            }
+            if (digitalRoot>9)
+            {
+                DigitalRoot(digitalRoot);
+            }
+            return digitalRoot;
+        }
+    }
+}
