85f5a84b6ca3dcba922d0dd86ed272da6bdb4078
Zachary Silvis <60155477+Bandit254@users.noreply.github.com>
2020-03-20 19:07:56 -0700

    Added a project that removes every other item from an array of objects

---
+++ RemovingElements/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace RemovingElements
+{
+    class Program
+    {
+        /*
+         * Take an array and remove every second element out of that array. 
+         * Always keep the first element and start removing with the next element.
+         * None of the arrays will be empty, so you don't have to worry about that!
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Remove every other element from an array, staring with the second element.\n");
+            object[] arr = new object[] { "Keep", "Remove", "Keep", "Remove", "Keep" ,"Remove", "Keep"};
+            object[] trimmedArray = RemoveElements(arr);
+            PrintArray(trimmedArray);
+
+        }
+
+        public static object[] RemoveElements(object[] arr)
+        {
+            object[] trimmedArray = new object[(arr.Length+1)/ 2];
+            for (int i = 0; i < trimmedArray.Length; i++)
+            {
+                trimmedArray[i] = arr[i * 2];
+            }              
+            return trimmedArray;
+        }
+
+        public static void PrintArray(object[] inputArray)
+        {
+            foreach (var x in inputArray)
+            {
+                Console.Write($"{x} ");
+            }
+        }
+    }
+}
