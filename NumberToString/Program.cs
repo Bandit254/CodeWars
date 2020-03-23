0ca2bf9f2e7153f744be05194038715d33955e3d
Zachary Silvis <60155477+Bandit254@users.noreply.github.com>
2020-03-23 14:47:22 -0700

    Added a project that properly converts an integer to a string

---
+++ NumberToString/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NumberToString
+{
+    class Program
+    {
+        /*The code gives an error!
+        Kata.A = 123.ToSTring();
+        Fix it!
+        */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Convert a number to a string.\n");
+
+            string A = 123.ToString();
+
+
+        }
+    }
+}
