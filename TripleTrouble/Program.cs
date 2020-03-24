68fca51c58d49feeb07f24e890118d293ccdfef4
Zachary Silvis <60155477+Bandit254@users.noreply.github.com>
2020-03-24 14:58:12 -0700

    Added a project that creates a string to represent he combination, in sequence, of all the letters in three input strings.

---
+++ TripleTrouble/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace TripleTrouble
+{
+    class Program
+    {
+        /*
+         * Create a function that will return a string that combines all of the letters of the three inputed strings in groups. 
+         * Taking the first letter of all of the inputs and grouping them next to each other. 
+         * Do this for every letter, see example below!
+         * E.g. Input: "aa", "bb" , "cc" => Output: "abcabc"
+         * Note: You can expect all of the inputs to be the same length.
+         * */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a function that combines the first (and each subsequent) letters of three uniform-length input strings.\n");
+            string one = "aa";
+            string two = "bb";
+            string three = "cc";
+            Console.WriteLine($"The combination, in sequence, of all the letters in the three strings is: {TripleTrouble(one, two, three)}.\n");
+        }
+
+        public static string TripleTrouble(string one, string two, string three)
+        {
+            string tripleString="";
+            for (int i = 0; i < one.Length; i++)
+            {
+                tripleString += one[i];
+                tripleString += two[i];
+                tripleString += three[i];
+            }
+            return tripleString;
+        }
+    }
+}
