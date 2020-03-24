700318eca930ede1e670050349d1ec53fdff145d
Zachary Silvis <60155477+Bandit254@users.noreply.github.com>
2020-03-24 16:36:37 -0700

    Added a project that doubles a given integer

---
+++ CodeUnderPressureOne/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CodeUnderPressureOne
+{
+    class Program
+    {
+        /*Code as fast as you can! You need to double the integer and return it.*/
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Double the given integer!\n");
+            int n = 4;
+            Console.WriteLine($"The double of {n} is {DoubleInteger(n)}.\n");
+        }
+
+        public static int DoubleInteger(int n)
+        {
+            int doubledInteger = 2 * n;
+            return doubledInteger;
+        }
+    }
+}
