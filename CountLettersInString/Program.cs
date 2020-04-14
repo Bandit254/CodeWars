9b7f70a1113d1cedd34123ea491cfcb261f9323d
Zachary Silvis <60155477+Bandit254@users.noreply.github.com>
2020-04-14 16:15:49 -0700

    Added a project to build a string that contains each letter that appears in an input string and the number of times each letter appears in the input string. Added an additional project to return a planet's name when given the planet's position in the solar system.

---
+++ CountLettersInString/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CountLettersInString
+{
+    class Program
+    {
+        /*
+         * Take a string str and return a string that is made up of the number of occurances of each english letter in str, 
+         * followed by that letter. The string shouldn't contain zeros; leave them out.
+         * An empty string, or one with no letters, should return an empty string.
+         * Notes:
+            Ignore all case
+            str will never be null
+            Examples
+            "This is a test sentence."  =>  "1a1c4e1h2i2n4s4t"
+            ""  =>  ""
+            "555"  =>  ""
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Return a string containing the each letter from an input string and the number of times that letter appears.\n");
+            string str = "This is a test sentence.";
+            string result = StringLetterCount(str);
+            Console.WriteLine($"{result}");
+        }
+        public static string StringLetterCount(string str)
+        {
+            string result = "";
+            str = str.ToLower();
+            string sortedString = "";
+            List<string> sortedList = new List<string>();
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i]<=122&&str[i]>=97)
+                {
+                    string currentLetter = Convert.ToString(str[i]);
+                    sortedList.Add(currentLetter);
+                }
+            }
+            sortedList.Sort();
+            for (int i = 0; i < sortedList.Count; i++)
+            {
+                sortedString += sortedList[i];
+            }
+            List<string> letterCounts = new List<string>();
+            for (int i = 0; i < sortedString.Length; i++)
+            {
+                if (sortedString[i]<=122 && sortedString[i]>=97)
+                {
+                    char currentChar = sortedString[i];
+                    int count = 1;
+                    for (int j = i+1; j < sortedString.Length; j++)
+                    {
+                        if (sortedString[j]==currentChar)
+                        {
+                            count++;
+                        }
+                    }
+                    string countString = Convert.ToString(count);
+                    string currentLetter = Convert.ToString(currentChar);
+                    if (letterCounts.Contains(currentLetter)==false)
+                    {
+                        letterCounts.Add(countString);
+                        letterCounts.Add(currentLetter);
+                    }
+                }
+            }
+            for (int i = 0; i < letterCounts.Count; i++)
+            {
+                result += letterCounts[i];
+            }
+            return result;
+        }
+    }
+}
---
+++ GetPlanetName/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace GetPlanetName
+{
+    class Program
+    {
+        //The function is not returning the correct values. Can you figure out why?
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fix the code so that the switch case appropriately returns the planet name associated with the planet's order in the solar system!");
+            Console.WriteLine($"{GetPlanetName(3)}");
+        }
+        public static string GetPlanetName(int id)
+        {
+            string name;
+            switch (id)
+            {
+                case 1:
+                    name = "Mercury";
+                    break;
+                case 2:
+                    name = "Venus";
+                    break;
+                case 3:
+                    name = "Earth";
+                    break;
+                case 4:
+                    name = "Mars";
+                    break;
+                case 5:
+                    name = "Jupiter";
+                    break;
+                case 6:
+                    name = "Saturn";
+                    break;
+                case 7:
+                    name = "Uranus";
+                    break;
+                case 8:
+                    name = "Neptune";
+                    break;
+                default:
+                    name = "Pluto";
+                    break;
+            }
+            return name;
+        }
+    }
+}
