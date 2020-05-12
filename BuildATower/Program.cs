bfed7dfb0d7aec2a76b4900e69829b38f1fc5cb9
Zachary Silvis <60155477+Bandit254@users.noreply.github.com>
2020-05-12 12:46:11 -0700

    Added a project that builds a tower from a string array

---
+++ BuildATower/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace BuildATower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Build a tower with a given number of floors.\n");
+            int nFloors = 6;
+            string[] tower = BuildATower(nFloors);
+            foreach (string s in tower)
+            {
+                Console.WriteLine($"'{s}'");
+            }
+
+        }
+        public static string[] BuildATower(int nFloors)
+        {
+            string[] tower = new string[nFloors];
+            int floorWidth = 2 * nFloors - 1;
+            int numStars = 1;
+            int sideSpaces = (floorWidth-1) / 2;
+            for (int i = 0; i < nFloors; i++)
+            {
+                string currentFloor = "";
+                for (int k = 0; k < sideSpaces; k++)
+                {
+                    currentFloor += ' ';
+                }
+                for (int h = 0; h < numStars; h++)
+                {
+                    currentFloor += '*';
+                }
+                for (int k = 0; k < sideSpaces; k++)
+                {
+                    currentFloor += ' ';
+                }
+                tower[i] = currentFloor;
+                sideSpaces--;
+                numStars += 2;
+            }
+            return tower;
+        }
+    }
+}
