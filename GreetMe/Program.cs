7ae99a0ca47990777ca17f3fe1febf6752b1fb73
Zachary Silvis <60155477+Bandit254@users.noreply.github.com>
2020-05-29 13:33:45 -0600

    Added a project that accepts a name, formats the name, and generates a greeting that includes the formatted name

---
+++ GreetMe/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GreetMe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.\n");
+            string name = "riley";
+            string name1 = "Jack";
+            Console.WriteLine($"{Greet(name)}\n");
+            Console.WriteLine($"{Greet(name1)}\n");
+        }
+
+        public static string Greet(string name)
+        {
+            string formattedName = "";
+            formattedName += Char.ToUpper(name[0]);
+            for (int i = 1; i < name.Length; i++)
+            {
+                formattedName += Char.ToLower(name[i]);
+            }
+            string finalGreeting = "Hello " + formattedName + "!";
+            return finalGreeting;
+        }
+    }
+}
