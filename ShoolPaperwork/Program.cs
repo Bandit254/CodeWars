85c1dc1177eb9e38cb6f06a46a24649df0b9aeff
Zachary Silvis <60155477+Bandit254@users.noreply.github.com>
2020-06-05 15:27:55 -0600

    First project in the Code Wars Beginner Series

---
+++ ShoolPaperwork/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ShoolPaperwork
+{
+    class Program
+    {
+        /*Your classmates asked you to copy some paperwork for them. 
+         * You know that there are 'n' classmates and the paperwork has 'm' pages.
+         * Your task is to calculate how many blank pages do you need.
+         * Example:
+         * Paperwork(5, 5) == 25
+         * Note: if n < 0 or m < 0 return 0!
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a function to multiply two numbers together.\n");
+            Console.WriteLine($"{Paperwork(5, 5)}");
+        }
+        public static int Paperwork(int n, int m)
+        {
+            if (n<0 || m<0)
+            {
+                return 0;
+            }
+            return n * m;
+        }
+    }
+}
