37580de6e8b54c3c43951c56ee322ec2e40fad14
Zachary Silvis <60155477+Bandit254@users.noreply.github.com>
2020-06-08 12:02:58 -0600

    Beginner - write a function that calculates a person's BMI based on their height and weight, and then returns the category of the BMI index.

---
+++ CalculateBMI/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CalculateBMI
+{
+    class Program
+    {
+        /*Write function bmi that calculates body mass index (bmi = weight / height ^ 2).
+         * if bmi <= 18.5 return "Underweight"
+         * if bmi <= 25.0 return "Normal"
+         * if bmi <= 30.0 return "Overweight"
+         * if bmi > 30 return "Obese"
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a function that calculates a person's BMI, given their height and weight.\n");
+
+        }
+        public static string Bmi(double weight, double height)
+        {
+            string bmiCategory="";
+            double bmi = weight / (Math.Pow(height, 2));
+            if (bmi<=18.5)
+            {
+                bmiCategory = "Underweight";
+            }
+            else if (bmi <= 25.0)
+            {
+                bmiCategory = "Normal";
+            }
+            else if (bmi <= 30.0)
+            {
+                bmiCategory = "Overweight";
+            }
+            else if (bmi > 30.0)
+            {
+                bmiCategory = "Obese";
+            }
+            return bmiCategory;
+        }
+    }
+}
