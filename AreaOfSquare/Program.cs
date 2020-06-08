97dd221f19f8e4ba0b1ad90b25eb84250bc0c693
Zachary Silvis <60155477+Bandit254@users.noreply.github.com>
2020-06-08 11:54:37 -0600

    Beginner - calculate the area of a square when given only the length of a circular arc that starts and ends in two corners of the square (ie, the radius is equal to the side length)

---
+++ AreaOfSquare/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AreaOfSquare
+{
+    class Program
+    {
+        /*Complete the function that calculates the area of a square, 
+         * when the length of the circular arc A is given as the input. 
+         * Return the result rounded to two decimals.
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculate the area of a square given the length of a circular arc that runs from one corner of the circle to the other.\n");
+            Console.WriteLine($"Given an arc of 2, the area of the circle is {SquareArea(2)}.\n");
+            Console.WriteLine($"Given an arc of 0, the area of the circle is {SquareArea(0)}.\n");
+            Console.WriteLine($"Given an arc of 14.05, the area of the circle is {SquareArea(80)}.\n");
+        }
+        public static double SquareArea(double A)
+        {
+            double radius = (4 * A) / (2 * Math.PI);
+            double area = radius * radius;
+            double result = Math.Round(area, 2);
+            return result;
+        }
+    }
+}
