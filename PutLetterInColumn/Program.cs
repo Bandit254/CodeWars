269c3937360a092d18c365b3307f008b36e62b69
Zachary Silvis <zachary.w.silvis@outlook.com>
2021-01-01 12:30:24 -0700

    Added a project to place aletter in a given column, and a second project to calculate the number of squares that can be made form a given rectangle.

---
+++ PutLetterInColumn/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace PutLetterInColumn
+{
+    class Program
+    {
+        /*
+          Create a function that takes index [0, 8] and a character. 
+          It returns a string with columns. Put character in column marked with index.
+          Ex.: index = 2, character = 'B'
+            | | |B| | | | | | |
+             0 1 2 3 4 5 6 7 8
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Create a function that takes index [0, 8] and a character. It returns a string with columns. Put the character in the column indicated by the given index.");
+            Console.WriteLine($"{BuildRowText(2, 'A')}");
+            Console.WriteLine($"{BuildRowText(0, 'A')}");
+            Console.WriteLine($"{BuildRowText(8, 'A')}");
+        }
+
+        public static string BuildRowText(int index, char character)
+        {
+            string result = "|";
+            for (int i = 0; i < 9; i++)
+            {
+                if (i == index)
+                {
+                    result += character;
+                    result += '|';
+                }
+                else
+                {
+                    result += " |";
+                }                
+            }
+            return result;
+        }
+    }
+}
---
+++ RectanglesIntoSquares/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace RectanglesIntoSquares
+{
+    class Program
+    {
+        /*
+         * The drawing below gives an idea of how to cut a given "true" rectangle into squares ("true" rectangle meaning that the two dimensions are different).
+         * Can you translate this drawing into an algorithm?
+
+           You will be given two dimensions:
+           
+           a positive integer length (parameter named lng)
+           a positive integer width (parameter named wdth)
+
+           You will return an array or list with the size of each of the squares:
+
+             sqInRect(5, 3) should return [3, 2, 1, 1]
+             sqInRect(3, 5) should return [3, 2, 1, 1]
+           
+           Notes:
+           lng == wdth as a starting case would be an entirely different problem and the drawing is planned to be interpreted with lng != wdth.
+           When the initial parameters are so that lng == wdth, the solution [lng] would be the most obvious but not in the spirit of this kata so, in that case, return None/nil/null/Nothing
+         * 
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a function that determines how many squares can be made out of a rectangle, and returns a list/array containing the side lengths of each square. \n");
+            int[] resultArray = RectangleToSquares(5, 3);
+            foreach (int side in resultArray)
+            {
+                Console.Write($"{side} ");
+            }
+        }
+
+        public static int[] RectangleToSquares(int lng, int wdth)
+        {
+            if (lng == wdth)
+            {
+                return null;
+            }
+            List<int> resultList = new List<int>();
+            int startArea = lng * wdth;
+            while (startArea > 0)
+            {
+                int longSide;
+                int shortSide;
+                if (lng >= wdth)
+                {
+                    longSide = lng;
+                    shortSide = wdth;
+                }
+                else
+                {
+                    longSide = wdth;
+                    shortSide = lng;
+                }
+                resultList.Add(shortSide);
+                int intermediateArea = shortSide * shortSide;
+                if (lng >= wdth)
+                {
+                    lng -= wdth;
+                }
+                else
+                {
+                    wdth -= lng;
+                }
+                startArea -= intermediateArea;
+            }
+            int[] resultArray = resultList.ToArray();
+            return resultArray;
+
+        }
+    }
+}
