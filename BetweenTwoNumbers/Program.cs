c2a4e49000d11b381076fa18d24dec520aa76816
Zachary Silvis <zachary.w.silvis@outlook.com>
2021-02-02 21:43:48 -0500

    Added a function that returns and array with the numbers that fall between a given starting and ending number.

---
+++ BetweenTwoNumbers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace BetweenTwoNumbers
+{
+    class Program
+    {
+        /*
+         * Build a function that can get all the integers between two given numbers.
+           Example:
+           (2,9)
+           Should give you this output back:
+           [ 3, 4, 5, 6, 7, 8 ]
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Build a function that can get all the integers between two given numbers.\n");
+            foreach (int num in Range(210, 210))
+            {
+                Console.Write($"{num} ");
+            }
+        }
+
+        public static int[] Range(int startNum, int endNum)
+        {
+            if (startNum == endNum)
+            {
+                return new int[0]; 
+            }
+            int numMissing = endNum - startNum - 1;
+            int nextNum = startNum + 1;
+            int[] between = new int[numMissing];
+            for (int i = 0; i < numMissing; i++)
+            {
+                between[i] = nextNum;
+                nextNum++;
+            }
+            return between;
+        }
+    }
+}
