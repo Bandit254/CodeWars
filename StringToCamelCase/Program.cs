f9217dbd7d1f46cf10b267348e91b33ac3d92c43
Zachary Silvis <zachary.w.silvis@outlook.com>
2021-02-23 19:23:30 -0500

    Added a project that takes a delimited string, removes the delimiters, and returns the string in camelCase.

---
+++ StringToCamelCase/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Text;
+
+namespace StringToCamelCase
+{
+    class Program
+    {
+        /*
+         Complete the method/function so that it converts dash/underscore delimited words into camel casing. 
+         The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case).
+
+         Examples:
+         "the-stealth-warrior" gets converted to "theStealthWarrior"
+         "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Convert a '-' or '_' delimited string to Camel Case (unless the first letter was capitalized already) with no spaces between words.\n");
+            Console.WriteLine($"{ToCamelCase("the-stealth-warrior")}");
+            Console.WriteLine($"{ToCamelCase("The_Stealth_Warrior")}");
+            Console.WriteLine($"{ToCamelCase("dxmvojpaqw_qjaoyoxnqs-Mmxfjdcagb")}");
+            //string[] dashesFirst = "the-stealth-warrior_Looks_Cool".Split('-');
+            //foreach (string str in dashesFirst)
+            //{
+            //    Console.WriteLine($"{str}\n");
+            //}
+            //string[] underscoresFirst = "the_stealth_warrior-Looks-Cool".Split('_');
+            //foreach (string str in underscoresFirst)
+            //{
+            //    Console.WriteLine($"{str}\n");
+            //}
+            //string[] mixedUnder = "the_stealth-warrior-Looks_Cool".Split('_');
+            //foreach (string str in mixedUnder)
+            //{
+            //    Console.WriteLine($"{str}\n");
+            //}
+            //string[] mixedDash = "the-stealth_warrior-Looks_Cool".Split('_');
+            //foreach (string str in mixedDash)
+            //{
+            //    Console.WriteLine($"{str}\n");
+            //}
+            //string[] splitSimultaneously = "the-stealth_warrior-Looks_Cool".Split('_', '-');
+            //foreach (string str in splitSimultaneously)
+            //{
+            //    Console.WriteLine($"{str}\n");
+            //}
+
+        }
+
+        public static string ToCamelCase(string str)
+        {
+            string[] strings = str.Split('_', '-');
+            for (int i = 0; i < strings.Length; i++)
+            {
+                string currentWord = strings[i];
+                char firstLetter = currentWord[0];
+                if (i == 0 && (97 <= firstLetter && firstLetter <= 122))
+                {
+                    continue;
+                }
+                else if (i == 0 && (65 <= firstLetter && firstLetter <= 90))
+                {
+                    continue;
+                }
+                else if (97 <= firstLetter && firstLetter <= 122)
+                {
+                    int firstChar = (int)firstLetter;
+                    firstChar -= 32;
+                    firstLetter = (char)firstChar;
+                    char[] letters = currentWord.ToCharArray();
+                    letters[0] = firstLetter;
+                    strings[i] = new string(letters);
+                }
+            }
+            string camelCase = "";
+            foreach (string word in strings)
+            {
+                camelCase += word;
+            }
+            return camelCase;
+        }
+    }
+}
---
+++ WeightForWeight/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace WeightForWeight
+{
+    class Program
+    {
+        /*
+        My friend John and I are members of the "Fat to Fit Club (FFC)". John is worried because each month a list with the weights of members is published and each month he is the last on the list which means he is the heaviest.
+        I am the one who establishes the list so I told him: "Don't worry any more, I will modify the order of the list". It was decided to attribute a "weight" to numbers. The weight of a number will be from now on the sum of its digits.
+        For example 99 will have "weight" 18, 100 will have "weight" 1 so in the list 100 will come before 99.
+        Given a string with the weights of FFC members in normal order can you give this string ordered by "weights" of these numbers?
+
+        Example:
+        "56 65 74 100 99 68 86 180 90" ordered by numbers weights becomes: 
+        
+        "100 180 90 56 65 74 68 86 99"
+        
+        When two numbers have the same "weight", let us class them as if they were strings (alphabetical ordering) and not numbers:
+        
+        180 is before 90 since, having the same "weight" (9), it comes before as a string.
+        
+        All numbers in the list are positive numbers and the list can be empty.
+        
+        Note:
+        It may happen that the input string have leading, trailing whitespaces and more than a unique whitespace between two consecutive numbers
+         */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a function that orders a list of people's weights by a 'weight' calculated by the adding the digits of the person's weight.\n");
+
+
+        }
+
+        public static string orderWeight(string strng)
+        {
+            string weightedList = "";
+
+
+
+            return weightedList;
+        }
+    }
+}
