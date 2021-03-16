012765f7e80ef1d9089c0c45ecab973dee601a90
Zachary Silvis <zachary.w.silvis@outlook.com>
2021-03-16 14:25:00 -0400

    Added a program that takes a list of numbers and orders them by their "wight" (sum of digits). Note: doesn't account for all edge cases listed in CodeWars.

--- WeightForWeight/Program.cs
+++ WeightForWeight/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace WeightForWeight
 {
@@ -27,16 +28,114 @@ namespace WeightForWeight
         static void Main(string[] args)
         {
             Console.WriteLine("Write a function that orders a list of people's weights by a 'weight' calculated by the adding the digits of the person's weight.\n");
-
-
+            Console.WriteLine($"{orderWeight("103 123 4444 99 2000")}");
+            Console.WriteLine($"{orderWeight("2000 10003 1234000 44444444 9999 11 11 22 123")}");
         }
 
         public static string orderWeight(string strng)
         {
+            string[] splitStrng = strng.Split();
+            List<string> stringWeights = new List<string>();
+            for (int i = 0; i < splitStrng.Length; i++)
+            {
+                if (String.IsNullOrWhiteSpace(splitStrng[i]))
+                {
+                    continue;
+                }
+                stringWeights.Add(splitStrng[i]);
+            }
+            List<long> weights = new List<long>();
+            for (int i = 0; i < stringWeights.Count; i++)
+            {
+                long weightLng = Convert.ToInt64(stringWeights[i]);
+                long weight = 0;
+                while (weightLng > 0)
+                {
+                    weight += weightLng % 10;
+                    weightLng /= 10;
+                }
+                weights.Add(weight);
+            }
+            for (int i = 0; i < weights.Count-1; i++)
+            {
+                for (int j = i+1; j < weights.Count; j++)
+                {
+                    if (weights[j] < weights[i])
+                    {
+                        long tempWeight = weights[j];
+                        weights[j] = weights[i];
+                        weights[i] = tempWeight;
+                        string tempString = stringWeights[j];
+                        stringWeights[j] = stringWeights[i];
+                        stringWeights[i] = tempString;
+                    }
+                }
+            }
+            for (int i = 0; i < weights.Count-1; i++)
+            {
+                for (int j = i+1; j < weights.Count; j++)
+                {
+                    if (weights[j] == weights[i])
+                    {
+                        bool swapped = false;
+                        string numJ = stringWeights[j];
+                        string numI = stringWeights[i];
+                        if (numJ.Length < numI.Length && numI[0] == numI[0])
+                        {
+                            long tempWeight = weights[j];
+                            weights[j] = weights[i];
+                            weights[i] = tempWeight;
+                            string tempString = stringWeights[j];
+                            stringWeights[j] = stringWeights[i];
+                            stringWeights[i] = tempString;
+                        }
+                        else
+                        {
+                            for (int k = 0; k < numJ.Length; k++)
+                            {
+                                for (int l = 0; l < numI.Length; l++)
+                                {
+                                    if (numJ[k] < numI[l])
+                                    {
+                                        long tempWeight = weights[j];
+                                        weights[j] = weights[i];
+                                        weights[i] = tempWeight;
+                                        string tempString = stringWeights[j];
+                                        stringWeights[j] = stringWeights[i];
+                                        stringWeights[i] = tempString;
+                                        swapped = true;
+                                    }
+                                    else
+                                    {
+                                        break;
+                                    }
+                                    if (swapped)
+                                    {
+                                        break;
+                                    }
+                                }
+                                if (swapped)
+                                {
+                                    break;
+                                }
+                            }
+                        }
+                        
+                    }
+                }
+            }
             string weightedList = "";
-
-
-
+            for (int i = 0; i < stringWeights.Count; i++)
+            {
+                if (i == stringWeights.Count - 1)
+                {
+                    weightedList += $"{stringWeights[i]}";
+                }
+                else
+                {
+                    weightedList += $"{stringWeights[i]} ";
+                }
+            }
             return weightedList;
         }
     }
